9464bc3ebae4c5a9836b4de8f4b20826864c0df6
isaquetdiniz <itd@cin.ufpe.br>
2022-10-21 09:40:46 -0300

    feat: add default entity

--- src/domain/entities/food.entity.go
+++ src/domain/entities/food.entity.go
@@ -1,7 +1,28 @@
 package entities
 
+import "fmt"
+
 type Food struct {
-	Entity
-	Category
-	NutritionalInformations
+	*Entity
+	*Category
+	*NutritionalInformations
+}
+
+func NewFood(
+	name string,
+	category *Category,
+	nutritionalInformations *NutritionalInformations,
+	options ...Option,
+) *Food {
+	f := &Food{
+		Entity:                  NewEntity(name, options...),
+		Category:                category,
+		NutritionalInformations: nutritionalInformations,
+	}
+
+	return f
+}
+
+func (e Food) String() string {
+	return fmt.Sprintf("[ID]: %s\n[Name]: %s\n[Category]: %s\n[Protein]: %d\n[Carb]: %d\n[Lipid]: %d\n[CreatedAt]: %s\n[UpdatedAt]: %s\n", e.ID, e.Name, e.Category.Name, e.NutritionalInformations.Protein, e.NutritionalInformations.Carbohydrate, e.NutritionalInformations.Lipid, e.CreatedAt, e.UpdatedAt)
 }
--- src/main.go
+++ src/main.go
@@ -6,14 +6,25 @@ import (
 	"log"
 	"net/http"
 	"taco-api/src/domain/entities"
-	"time"
 )
 
 func main() {
-	c := entities.Category{
-		Entity: entities.Entity{Id: "id", Name: "Cereal", CreatedAt: time.Now().Local(), UpdatedAt: time.Now().Local()}}
 
-	fmt.Println(c)
+	category := entities.NewCategory("Cereal")
+
+	nutritionalInformations := &entities.NutritionalInformations{
+		Protein:      10,
+		Carbohydrate: 20,
+		Lipid:        5,
+	}
+
+	f := entities.NewFood(
+		"Arroz",
+		category,
+		nutritionalInformations,
+	)
+
+	fmt.Println(f)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
